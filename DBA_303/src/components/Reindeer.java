546154db7ad2fb3e43867060dfd9bb3ef49a5ba5
capcrz12 <carlosperezcruz01@gmail.com>
2023-12-09 18:18:34 +0100

    Refactorizado enum Reindeer a class Reindeer

--- DBA_303/src/components/Reindeer.java
+++ DBA_303/src/components/Reindeer.java
@@ -5,16 +5,87 @@
  */
 package components;
 
+
 /**
- * @brief   Enumerate all Santa's lost reindeers 
+ * @brief This class models each tile inside the Map.
  */
-public enum Reindeer {
-    BLITZEN,
-    COMET,
-    CUPIDO,
-    DANCER,
-    DASHER,
-    DONNER,
-    PRANCER,
-    VIXEN,
+public class Reindeer {
+    
+    /// Name of the reindeer
+    private final Name name;
+    
+    /// State of the reindeer
+    private State state;
+    
+    /// Position of the reindeer in the map
+    private Position pos;
+    
+    /**
+     * Reindeer Constructor
+     * @param name The reindeer name
+     */
+    public Reindeer(Name name) {
+        this.name = name;
+        this.state = State.UNKNOWN;
+        this.pos = new Position(1,1);
+    }
+    
+    /**
+     * Reindeer name getter
+     * @return the reindeer name
+     */
+    public Name getName() { return this.name; }
+    
+    /**
+     * Reindeer name getter
+     * @return the reindeer name
+     */
+    public State getState() { return this.state; }
+    
+        /**
+     * Reindeer position getter
+     * @return the reindeer pos
+     */
+    public Position getPosition() { return this.pos; }
+    
+    /**
+     * Reindeer state setter
+     * 
+     */
+    public void setState(State state) { 
+        this.state = state; 
+    }
+    
+    /**
+     * Reindeer position setter
+     * 
+     */
+    public void setPosition(Position pos) { 
+        this.pos = pos; 
+    }
+    
+    
+    /**
+     * Enum all Santa's lost reindeers 
+     */
+    public enum Name {
+        BLITZEN,
+        COMET,
+        CUPIDO,
+        DANCER,
+        DASHER,
+        DONNER,
+        PRANCER,
+        VIXEN
+    }
+    
+    /**
+     * Enum reindeer states
+     */
+    public enum State {
+        UNKNOWN,        // When Elf hasn't talk with Rudolph yet
+        KNOWN,          // When Sensor doesn't know the reindeer pos
+        CURRENT,        // When Sensor knows the reindeer pos
+        FOUND,          // It has been found, so it dissappears from the map
+    }
 }
